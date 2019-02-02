15e1c40ffbf60c37823e5b3ba6f20051871534e6
Keagan Bradley <keagan.jbradley@gmail.com>
2019-02-02 20:35:49 +0800

    Initial Commit

---
+++ Udder 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder 2.0
+//
+//  Created by Keagan Bradley on 2/02/19.
+//  Copyright © 2019 Keagan Bradley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
